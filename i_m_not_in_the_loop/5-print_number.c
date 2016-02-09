033f13dadcd1f1d4d0381628d681afed06a443b7
Sravanthi Sinha <sravani.sinha@gmail.com>
2016-02-08 17:14:59 -0800

    Fixed code to work with min of int

--- i_m_not_in_the_loop/5-print_number.c
+++ i_m_not_in_the_loop/5-print_number.c
@@ -1,5 +1,5 @@
 int print_char(char);
-int power(int  x,unsigned int y)
+long power(unsigned long  x,int y)
 {
   if( y == 0)
     return 1;
@@ -7,45 +7,43 @@ int power(int  x,unsigned int y)
     return power(x, y/2)*power(x, y/2);
   else
     return x*power(x, y/2)*power(x, y/2);
-
 }
-int  get_len (int  value){
+int  get_len (unsigned long  value){
    int l=1;
-  while(value>9){
-    l++; 
-    value/=10;
-  }
+   while(value>9)
+    {
+     l++;
+     value/=10;
+    }
   return l;
 }
-void my_print(int x){
-  int result=0;
-  int len=0;
-  int p=0;
+void my_print(unsigned long x){
+  long result;
+  int len,p;
   len=get_len(x);
   p=len;
-  if (x==0)print_char(48);
-  else{
-    while(p<=len && x!=0)
-      {
-	result=x/power(10,p-1);
-	print_char(result+48);
-	x=x-(result*power(10,p-1));
-	p--;
-	len=get_len(x);
-	while(p>len){
-	  print_char(48);
-	  p--;
-	}
-      }
-  }
+  while(p<=len && x!=0)
+    {
+        result=x/power(10,p-1);
+        print_char(result+48);
+        x=x-(result*power(10,p-1));
+        p--;
+        len=get_len(x);
+        while(p>len)
+          {
+            print_char(48);
+            p--;
+          }
+    }
 }
 void print_number(int x)
 {
   if (x==0)print_char(48);
-  else if(x<0){
+  else if(x<0)
+    {
     print_char(45);
-    my_print(x*-1);
-  }
+    my_print((unsigned long)x*-1);
+    }
   else{
     my_print(x);
   }
