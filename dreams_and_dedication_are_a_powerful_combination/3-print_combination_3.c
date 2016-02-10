8a0088e54e15276053fed7bcf5dd7e7ab724fcaf
SravanthiSinha <sravani.sinha@gmail.com>
2016-02-10 22:24:39 +0000

    initial commit

---
+++ dreams_and_dedication_are_a_powerful_combination/3-print_combination_3.c
@@ -0,0 +1,29 @@
+#include "my_functions.h"
+
+void print_combination_3(void);
+
+void print_combination_3(void){
+  int i=0;
+  int j=0;
+  int k=0;
+  for(i=0;i<=9;i++)
+    {
+      for (j=1;j<=9;j++)
+	{
+	  for (k=2;k<=9;k++)
+	    {
+	      if(i<=j && i!=j && j<=k && j!=k)
+		{
+		  print_number(i);
+		  print_number(j);
+		  print_number(k);
+		  if((i<7) && (j<=8) && (k<=9))
+		    {
+		      print_char(',');
+		      print_char(' ');
+		    }
+		}
+	    }
+	}
+    }
+}
