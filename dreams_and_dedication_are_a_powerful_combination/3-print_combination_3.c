0789da5c0033eb65f1461df15edc2db5e2753069
SravanthiSinha <sravani.sinha@gmail.com>
2016-02-15 02:33:56 +0000

    included commits

--- dreams_and_dedication_are_a_powerful_combination/3-print_combination_3.c
+++ dreams_and_dedication_are_a_powerful_combination/3-print_combination_3.c
@@ -1,9 +1,16 @@
 #include "my_functions.h"
 
-void print_combination_3(void);
+/* The function print_combination_1 takes no arguments
+ * and prints it on one line - all possible
+ * combinations of three-digit numbers in 
+ * ascending order seperated by comma followe by a space. 
+ * takes no argument
+ * returns nothing (void)
+ */
 
 void print_combination_3(void){
   int i,j,k;
+
   for(i=0;i<=9;i++)
     {
       for (j=1;j<=9;j++)
