c7f0672616e282d357b2dd3b4b5e67f341f9d59d
SravanthiSinha <sravani.sinha@gmail.com>
2016-02-15 02:59:29 +0000

    included comments

--- dreams_and_dedication_are_a_powerful_combination/4-print_combination_4.c
+++ dreams_and_dedication_are_a_powerful_combination/4-print_combination_4.c
@@ -1,16 +1,18 @@
 #include "my_functions.h"
 
-void print_combination_4(void);
+/* The function print_combination_4 takes no arguments
+ * and prints - all possible
+ * combinations of two two-digit numbers seperated by comma followe by a space. 
+ * takes no argument
+ * returns nothing (void)
+ */
 
 void print_combination_4(void){
   int i,j, k,l,a,b;
   int end=9;
   for(i=0;i<=end;i++)
-    {
-      for (j=0;j<=end;j++)
-	{
-	  for (k=0;k<=end;k++)
-	    {
+      for (j=0;j<=end;j++)	
+	  for (k=0;k<=end;k++)	    
 	      for (l=0;l<=end;l++)
 		{
 		  a=i*10+j;
@@ -29,7 +31,5 @@ void print_combination_4(void){
 			}
 		    }
 		}
-	    }
-	}
-    }
-}
+	    
+}       
