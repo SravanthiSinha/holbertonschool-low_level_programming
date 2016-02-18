152859d437a03a374443a9cb7636c421677c243d
SravanthiSinha <sravani.sinha@gmail.com>
2016-02-18 11:47:16 +0530

    initial commit

---
+++ keep_calm_and_love_programming/0-absolute.c
@@ -0,0 +1,6 @@
+int absolute(int n)
+{
+  if (n<0)
+    return -n;
+  else return n;
+}
---
+++ keep_calm_and_love_programming/0-main.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<limits.h>
+
+int absolute(int);
+
+int main(void)
+{
+  int n;
+
+  n = absolute(-1);
+  printf("%d\n", n);
+  n = absolute(10);
+  printf("%d\n", n);
+  n = absolute(0);
+  printf("%d\n", n);
+  n = absolute(INT_MAX);
+  printf("%d\n", n);
+  n = absolute(INT_MIN + 1);
+  printf("%d\n", n);
+  n = absolute(INT_MIN);
+  printf("%d\n", n);
+  return (0);
+}
