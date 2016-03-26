9161d0a29a3ff45fc1f2b02e0a5ff6a0e6e91548
sravanthisinha <sravani.sinha@gmail.com>
2016-03-26 02:08:03 +0000

    initial commit

--- let_s_go_deeper/4-is_palindrome.c
+++ let_s_go_deeper/4-is_palindrome.c
@@ -3,23 +3,24 @@ int str_len(char *str)
   int len;
 
   len=0;
-  while(*(str+len)!='\0')
+  while( *(str+len) != '\0' )
     len++;
   return len;
 }
 
-int checkpalindrome(char *s, int mid)
+int checkpalindrome(char *s, int len, int count)
 {
-  if
-  
+  if(count >= len) return 1;
+  if(s[count] != s[len-count-1]) return 0;
+  return checkpalindrome(s,len,count+1);
 }  
 
 
 int is_palindrome(char *s)
 {  
-  int mid;
+  int len;
   
-  mid=str_len(s)/2;
-  if( mid %2 == 0)
-  retrun checkpalindrome(s,mid));
+  len=str_len(s);
+
+  return checkpalindrome(s,len,0);
 }
