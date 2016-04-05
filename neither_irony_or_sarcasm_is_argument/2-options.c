e093c0f2ade6e701cc9c31296bed91bf5a2e9166
SravanthiSinha <sravani.sinha@gmail.com>
2016-04-05 06:33:10 +0000

    fixed the code

--- neither_irony_or_sarcasm_is_argument/2-options.c
+++ neither_irony_or_sarcasm_is_argument/2-options.c
@@ -1,24 +1,57 @@
 #include <stdio.h>
 #include<string.h>
 
+
+
 int main(int argc, char *argv[])
 {
   int i;
+  unsigned int l,m;
+char options[1000];
+ char c[4];
+ char *s;
+ strcpy( options,"0");
 
   for(i=1;i<argc;i++)
     {
-      if(argv[i][0]=='-' && argv[i][1]=='-' && argv[i][2]=='\0')  
+      if(strcmp(argv[i],"--")==0)
 	{
-	  i=i+2;
+	  break;
 	}
       else if(argv[i][0]=='-' && argv[i][1]=='-')  
 	{
-	printf("%.*s\n",(int)strlen(argv[i]),argv[i]+2);
-	i++;
+	  s=argv[i];
+	  s +=2;	 
+	  if(!strstr(options,s))
+	    {  
+	      strcat(options,s);
+	      strcat(options,", ");
+	    }
 	}
       else if(argv[i][0]=='-')
-	printf("%.*s\n",(int)strlen(argv[i]),argv[i]+1);
-    }
-  
+	{
+	  s=argv[i];
+	  s +=1;
+	  m=strlen(s);
+	  l=1;
+	  while(l<=m)
+	    {
+	      c[0]=argv[i][l];
+	      c[1]=',';
+	      c[2]=' ';
+	      c[3]='\0';
+	      if(!strstr(options,c))
+		{
+		  strcat(options,c);
+		}
+	      l++;
+	      }
+	}
+
+	}
+
+  options[strlen(options)-2]='\0';
+  printf("%.*s\n",(int)strlen(options),options+1);
+
   return 0;
 }
