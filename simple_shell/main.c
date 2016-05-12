2da3d07a1a08b86ce3129c1835f5f265e7457798
SravaanthiSinha <sravani.sinha@gmail.com>
2016-05-12 22:57:36 +0000

    handled path

--- simple_shell/main.c
+++ simple_shell/main.c
@@ -11,52 +11,52 @@ int str_len(const char *s);
 int str_ncomp( char *s1, char*s2, int k);
 void print_prompt(char *s);
 int exe_exists(char *s);
-char **getpaths(char **env);
+char *getcommand(char **env, char *cmd);
+void print_number(int);
+int print_char(char);
+
 int main(int argc, char **argv, char **env)
 {
   char *command;
   char **args;
   pid_t pid;
   int status;
-  char **paths;
   char *path;
   int i;
   int exefound;
 
   i = 0;
-  exefound = 1;
-
+  exefound = 0;
+  path ='\0';
   while(1)
     {
-       print_prompt("cisfun$:");
+       print_prompt("shellisfun$:");
        command = read_line(0);
        args = string_split(command, ' ');
-
        if(str_ncomp(args[0], "exit", str_len("exit")) == 0)
 	    exit(0);	 
-       else if((pid = fork()) == -1);
+       
+       if((pid = fork()) == -1);
        else if(pid  == 0)
-	 {	  	  
-	   paths = getpaths(env);
-	   for(i = 0; paths[i] != NULL;i++)
+	 {	
+	   if(str_ncomp(args[0], "$?", str_len("$?")) == 0)
 	     {
-	       path = string_concat(string_concat(paths[i],"/"),args[0]);
-
-	       if(exe_exists(path))
-		 {
-		   if(execve(path,args,env) == -1)
-		     {
-		       exefound = 0;
-		     }
-		   return (0);
-		 }
+	       print_number(status);
+	       print_prompt("\n");
+	       return (0);
 	     }
-	   if(!exefound)
+	   else if(args[0][0] != '/')
+	     path = getcommand(env,args[0]);
+	   else
+	     path = args[0];
+	   if(execve(path,args,env) == -1)
 	     {
+	       print_prompt("shellisfun$:");
 	       print_prompt(args[0]);
 	       print_prompt(": command not found\n");	       	 
 	       return (0);
-	     }
+	      }
+	   return (0);
 	 }
        else
 	 {
@@ -66,12 +66,13 @@ int main(int argc, char **argv, char **env)
   return (0);
 }
 
-char **getpaths(char **env)
+char *getcommand(char **env , char *cmd)
 {
   char **paths;
   char *path;
   int i;
 
+  path = '\0';
     for ( i = 0 ; env[i]!= NULL;++i)
     {
       if(str_ncomp(env[i], "PATH=", str_len("PATH=")) == 0)
@@ -82,7 +83,16 @@ char **getpaths(char **env)
 	  break;
 	}	  
     }
-     return paths;
+    for(i = 0; paths[i] != NULL;i++)
+     {
+       path = string_concat(string_concat(paths[i],"/"),cmd);
+       if(exe_exists(path))
+	 {
+	   return path;
+	 }
+       path = '\0';
+     }	
+     return path;
 }
 
 int exe_exists(char *s)
@@ -96,10 +106,20 @@ int exe_exists(char *s)
     return(1);
 }
 
+void print_number(int n)
+{
+  if ( n > 10 )
+    print_number(n /10);
+  print_char(n % 10 +48);  
+}
+
+int print_char(char c)
+{
+  write(1,&c,1);
+}
 /*
  *  a function that concatenates two strings.
  */
-
 char *string_concat(char *s1, char *s2)
 {
  
