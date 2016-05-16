6b4cf816d3d22aa7a69b438f5f2a2e48f7c65abf
SravaanthiSinha <sravani.sinha@gmail.com>
2016-05-16 00:47:38 +0000

    store the status of the terminated process on the variable 0

--- simple_shell/main.c
+++ simple_shell/main.c
@@ -5,10 +5,12 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+#include <stdio.h>
 
 int exe_exists(char *s);
 char *getcommand(char **env, char *cmd);
 int exe_fork(char **env, char **argv);
+int handlespl(char **args, int status);
 
 int main(__attribute__((unused))int argc, __attribute__((unused))char **argv, char **env)
 {
@@ -16,7 +18,8 @@ int main(__attribute__((unused))int argc, __attribute__((unused))char **argv, ch
   char **args;
   pid_t pid;
   int status;
- 
+  
+  status =0;
   while(1)
     {
       /*Display a prompt*/
@@ -25,29 +28,47 @@ int main(__attribute__((unused))int argc, __attribute__((unused))char **argv, ch
        command = read_line(0);
        /*split the user input to find the command */
        args = string_split(command, ' ');
-       /*Terminates your program*/
-       if(str_ncomp(args[0], "exit", str_len("exit")) == 0)
-	    exit(0);	        
+
+       if (handlespl(args,status))
+	 continue;
+       
        /* create a process for the command to be executed*/
        if((pid = fork()) == -1)
 	 {
+	   perror("fork error");
+	   exit(EXIT_FAILURE);
 	 }
        else if(pid  == 0)
 	 {	
 	   /*return to the immediate parent once the command is executed*/
 	   return exe_fork(env, args);
 	 }
-        else
-	  {
-	    wait(&status);
-	    if(str_ncomp(args[0],"$?",str_len("$?")) == 0)
-	      {
-		print_number(status);
-		print_prompt("\n");
-	      }
-	  }
-    }    
+       else
+	 {
+	   wait(&status);
+	 }
+    }
   return (0);
+}
+
+int handlespl(__attribute__((unused)) char **args, __attribute__((unused)) int status)
+{
+  /*Terminates your program*/
+  if(str_ncomp(args[0], "exit", str_len("exit")) == 0)
+    exit(0);	        
+  else if((str_ncomp(args[0],"echo",str_len("echo")) == 0) && (str_ncomp(args[1],"$?",str_len("$?")) == 0))
+    {
+      print_number(status);
+      print_prompt("\n");
+      return 1;
+    }
+  else if((str_ncomp(args[0],"$?",str_len("$?")) == 0))
+    {
+      print_number(status);
+      print_prompt(": command not found\n");
+      return 1;
+    }
+  return 0;
 }
 
 /* execute the executables(those that can be
@@ -58,23 +79,24 @@ int main(__attribute__((unused))int argc, __attribute__((unused))char **argv, ch
 int exe_fork(char **env, char **args)
 {
   char *path;
+  int exe_status;
 
+  exe_status =0;
   path ='\0';
   if(args[0][0] != '/')
     path = getcommand(env,args[0]);
   else
     path = args[0];
-  if((execve(path,args,env) == -1))
+  exe_status = execve(path,args,env);
+  if(exe_status == -1)
     {
       if(str_ncomp(args[0],"$?",str_len("$?")) != 0)
 	{
-	  print_prompt("shellisfun$:");
 	  print_prompt(args[0]);
-	  print_prompt(": command not found\n");	       	 
+	  print_prompt(": command not found\n");        
 	}
-      return (1);
     }
-  return (0);
+  return (exe_status);
 }
  
 /* get the path of the executable for the user command*/
