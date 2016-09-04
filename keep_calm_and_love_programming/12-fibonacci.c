954c8e2e5505cead7d9ef008285bde52c0c5e9fd
sravanthisinha <sinha.sravanthi@gmail.com>
2016-09-05 02:33:41 +0530

    completed 100 fibonnaci

--- keep_calm_and_love_programming/12-fibonacci.c
+++ keep_calm_and_love_programming/12-fibonacci.c
@@ -1,26 +1,44 @@
 #include<stdio.h>
+#define n 98
 
-/* The function prints the  prints the first 50 Fibonacci numbers,
- * starting with 1 and 2, followed by a new line.
- * It takes no arguments argument (void) and
- * returns an int
- */ 
-int main(void)
-{
-unsigned  long  count,a,b,c;
-  a=0;
-  b=1;
-  c=0;
-
-  for (count=0;count<=98;count++)
+int main(void){
+  int f1_l,f2_l,f3_l,f1_c,f2_c,f3_c,f1_r,f2_r,f3_r,carry,carry2;
+  int no = n;
+  f1_l = 0;
+  f1_c = 0;
+  f1_r = 0;
+  f2_l = 0;
+  f2_c = 0;
+  f2_r = 1;
+  f3_l = 0;
+  f3_c = 0;
+  f3_r = 1;
+  while(no--)
     {
-      printf("%lu",a+b);
-      if(count<98) printf(", ");
-      c=a;
-      a=b;
-      b=c+b;      
+      carry = (f1_r + f2_r) / 100000000;
+      f3_r = (f1_r + f2_r) % 100000000;
+      f3_c = f1_c + f2_c +carry;            
+      if(f3_c > 0)
+	{
+	  carry2 = (f1_c + f2_c + carry) / 100000000;
+	  f3_c = (f1_c + f2_c +carry) % 100000000;
+	  f3_l = f1_l + f2_l +carry2;
+	  if(f3_l > 0)
+	      printf("%d%0*d%0*d",f3_l,8,f3_c,8,f3_r);
+	  else
+	    printf("%d%0*d",f3_c,8,f3_r);
+	}
+      else
+	  printf("%d",f3_r);
+      if(no > 0)
+	printf(", ");
+      f1_r = f2_r;
+      f2_r = f3_r;
+      f1_c = f2_c;
+      f2_c = f3_c;
+      f1_l = f2_l;
+      f2_l = f3_l;
     }
-	 printf("\n");
-
+  printf("\n");
   return 0;
 }
