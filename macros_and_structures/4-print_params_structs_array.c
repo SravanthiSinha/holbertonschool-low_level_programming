3243a42c3b08c5ad3610bedb41a843b8c1110d50
sravanthisinha <sravani.sinha@gmail.com>
2017-02-16 13:16:54 -0800

    modified print without using nwords

--- macros_and_structures/4-print_params_structs_array.c
+++ macros_and_structures/4-print_params_structs_array.c
@@ -1,68 +1,44 @@
-#include<stdlib.h>
 #include "param_struct.h"
-int nwords(char *s);
-int print_char(char c);
-
-void print_number(int n)
+int print_char(char);
+int str_len(char);
+void print_str(char *s)
 {
-  int count,i,temp,j,x;
-  temp=0; /*to store prev result*/
-  i=n;
-  for (count=0;i!=0;count++) /*count digits*/
-    i=i/10;
-  if (n<0)    /*if neg print '-'*/
-    print_char('-');
-  do{   /*do loop to work for 0*/
-    i=n;
-    x=count;
-    for(;x>1;x--){  /*start from beginning*/
-      i=i/10;
-    }
-    j=i-(temp*10);  /*subtract prev value*/
-    temp=i;        /*save prev value*/
-    if (j<0){  /*if neg digit make pos*/
-      j=j*(-1);
-    }
-    print_char(j+48); /*+48 for ascii*/
-    count--;
-  }
-  while(count>0);
-}
-
-void print_string(char *s)
-{
-  int i ;
-  
+  int i;
   i = 0;
-  while(s[i]!='\0')
+  while(s[i])
     {
       print_char(s[i]);
       i++;
-    }  
+    }
+}
+
+void print_num(int n)
+{
+  if(n>10)
+      print_num(n/10);
+  print_char(n%10 + 48);
 }
 
 void print_params_structs_array(struct Param *params)
 {
-  int i;
-  int j;
-  int x;
-
+  int i,x;
   i = 0;
-  while(params[i].str != NULL)
+  while(params[i].str)
     {
-      print_string(params[i].copy);
+      print_str(params[i].copy);
       print_char('\n');
-      print_number(params[i].length);
+      print_num(params[i].length);
       print_char('\n');
-      x = nwords(params[i].copy);
-      for(j = 0; j < x; j++)
+      x = 0;
+      while(params[i].tab[x])
 	{
-	  print_string(params[i].tab[j]);
-	  if( j < x -1)
+	  if(x != 0)
 	    {
 	      print_char(',');
 	      print_char(' ');
 	    }
+	  print_str(params[i].tab[x]);
+	  x++;
 	}
       print_char('\n');
       i++;
