7b86f97b8281fb9b7219217c17caaf16895a1138
sravanthisinha <sravani.sinha@gmail.com>
2017-02-17 13:00:08 -0800

    simplified funtionptr using typedef

--- mistakes_are_always_forgivable_if_one_has_the_courage_to_admit_them/2-calculator/get_op_func.c
+++ mistakes_are_always_forgivable_if_one_has_the_courage_to_admit_them/2-calculator/get_op_func.c
@@ -1,24 +1,19 @@
-#include <stdio.h>
-int op_add(int, int);
-int op_sub(int, int);
-int op_mul(int, int);
-int op_div(int, int);
-int op_mod(int, int);
+#include <stddef.h>
+typedef int (*funcPtr)(int,int);
+int op_add(int,int);
+int op_sub(int,int);
+int op_div(int,int);
+int op_mul(int,int);
+int op_mod(int,int);
 
-int (*get_op_func(char c))(int a, int b){
+int (*get_op_func(char c))(int ,int)
+{
   int i;
-
-  char options[5] = { '+', '-', '*','/', '%'};
-
-  int (*op_func[5])(int, int) = {op_add, op_sub, op_mul, op_div, op_mod};
-
-  for(i = 0;i < 5; i++)
-    {
-      if(c == options[i])
-	{
-	  return op_func[i];
-	}
-    }
-
-    return NULL;
+  char op[5] = {'+','-','*','/','%'};
+  //  int (*p[5])(int,int) ={op_add,op_sub,op_mul,op_div,op_mod};
+  funcPtr p[5] = {op_add,op_sub,op_mul,op_div,op_mod};
+  for(i = 0;i<5;i++)
+    if(c == op[i])
+      return *p[i];
+  return NULL;  
 }
