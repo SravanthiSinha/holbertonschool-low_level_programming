949aa0ff277c2a393adaf4608e13f8b2909fcb0a
Sravanthi Sinha <sinha.sravanthi@gmail.com>
2017-04-19 16:16:21 -0700

    fixing the style

--- 0x12-more_singly_linked_lists/2-add_nodeint.c
+++ 0x12-more_singly_linked_lists/2-add_nodeint.c
@@ -14,7 +14,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	node = malloc(sizeof(listint_t));
 	if (node == NULL)
-	       return (NULL);
+		return (NULL);
 	node->n = n;
 	node->next = *head;
 	*head = node;
