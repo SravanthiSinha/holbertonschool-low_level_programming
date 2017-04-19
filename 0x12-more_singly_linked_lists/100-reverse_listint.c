38a74cb07cc8bdfe94cfdad936e2850f66f3af70
sravanthisinha <sravani.sinha@gmail.com>
2017-04-19 16:59:17 -0700

    fixing typ in the fun name

--- 0x12-more_singly_linked_lists/100-reverse_listint.c
+++ 0x12-more_singly_linked_lists/100-reverse_listint.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
- * reverse_listint_tint - *reverses a listint_t linked list
+ * reverse_listint - *reverses a listint_t linked list
  * @head: Head node of type struct listint_t.
  *
  * Return: a pointer to the first node of the reversed list
