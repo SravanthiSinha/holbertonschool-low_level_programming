c838d56b1f993654bbfcd668953fc3eda80e033d
sravanthisinha <sravani.sinha@gmail.com>
2017-04-19 22:34:19 -0700

    included tests

--- printf/holberton.h
+++ printf/holberton.h
@@ -25,6 +25,7 @@ void swap(char *a, char *b);
 int is_number(char c, int option);
 int string_to_integer(char *s);
 int _printf(const char *format, ...);
+void print_o(va_list ap, int *p, int *i, const char *format);
 char *string_concat(char *dest, char *src);
 int str_len(const char *s);
 int str_ncomp(char *s1, char *s2, int k);
